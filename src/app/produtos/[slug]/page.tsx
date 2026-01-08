f2963b22efb42e4902fb8e292053a072e969dd5e
Dáviny Letícia <daviny.vidal@outlook.com>
2026-01-08 19:31:56 -0300

    a

--- src/app/produtos/[slug]/page.tsx
+++ src/app/produtos/[slug]/page.tsx
@@ -120,11 +120,11 @@ const ProductPage = () => {
         {product.gallery.map((img, index) => (
           <div 
             key={index} 
+            className="relative"
           >
             <img 
               src={img.url} 
-              alt={img.alt} 
-              className="object-cover transform group-hover:scale-105 transition-transform duration-500"
+              alt={img.alt}
             />
             {/* Overlay sutil no hover */}
             <div className="absolute inset-0 bg-gradient-to-t from-[#003366]/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
